332061db43d32d4739afbb2ebae457558304fff2
nuentoter <nuentoter@gmail.com>
2026-06-15 11:40:52 -0400

    Delete npc.py

--- npc.py
+++
@@ -1,8 +0,0 @@
-class NPC:
-    def __init__(self, name, x, y, predictability):
-        self.name = name
-        self.x = x
-        self.y = y
-        self.predictability = predictability
-        self.last_seen = None
-        self.behavior_signature = "A"
