d11aee2731614db23f431727f164675f8c99a0df
nuentoter <nuentoter@gmail.com>
2026-06-15 11:31:31 -0400

    Create world.py

---
+++ world.py
@@ -0,0 +1,16 @@
+A = Apartment
+R = Road
+S = Shop
+D = Depot
+Q = Quarry
+F = Fuel Station
+
+    0 1 2 3 4 5 6 7 8 9
+0   . . . . . . . . . .
+1   . A A A . . . Q Q .
+2   . A A A . . . Q Q .
+3   . . R R R R . . . .
+4   . . R S . R . F . .
+5   . . R . . R . . . .
+6   . . D D D . . . . .
+7   . . . . . . . . . .
